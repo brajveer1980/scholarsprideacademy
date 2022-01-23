469c43a751acada165938f682f2b30f39b09052e
Parents <Parents@DESKTOP-0QO3SJJ>
2022-01-23 14:18:17 -0600

    Updated new files

---
+++ eventgallery.js
@@ -0,0 +1,35 @@
+   var myString = `"<section class="col-md-12">\
+		<h2 class="ft-heading text-upper">Events Gallery</h2>\
+		<div class="carousel">\
+			<ul class="slides">\
+				<li>\
+					<div class="row">\
+						<div class="col-md-3">\
+							<div class="ft-item">\
+								<span class="ft-image"> <img src="images/photolib/img21072.jpeg" />\
+								</span>\
+							</div>\
+						</div>\
+						<div class="col-md-3">\
+							<div class="ft-item">\
+								<span class="ft-image"> <img src="images/photolib/img21073.jpeg" />\
+								</span>\
+							</div>\
+						</div>\
+						<div class="col-md-3">\
+							<div class="ft-item">\
+								<span class="ft-image"> <img src="images/photolib/img21074.jpeg" />\
+								</span>\
+							</div>\
+						</div>\
+						<div class="col-md-3">\
+							<div class="ft-item">\
+								<span class="ft-image"> <img src="images/photolib/img21075.jpeg" />\
+								</span>\
+							</div>\
+						</div>\
+					</div>\
+				</li>\
+			</ul>\
+		</div>\
+	</section>"`;
